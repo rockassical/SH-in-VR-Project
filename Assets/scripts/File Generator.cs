8031399d61004aba628f9e07d0777c068b8f5855
Alvaro-esteche <aesteche06@gmail.com>
2025-04-09 19:02:12 -0400

    Made the scripts to track the report function and a skeleton for the other tracking

---
+++ Assets/scripts/File Generator.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Palmmedia.ReportGenerator.Core.Common;
+using UnityEngine;
+
+public class FileGenerator : MonoBehaviour
+{
+    public static FileGenerator instance;
+    public static int CDTotal, ADTotal, CRTotal, ARTotal; //in order cue direct, Action direct, Cue report, Action report
+    public static int CDActual, ADActual, CRActual, ARActual; //in order cue direct, Action direct, Cue report, Action report
+    void Start()
+    {
+        instance = this;
+        createText();
+        saveFile();
+    }
+
+    void createText(){
+        String path = Application.dataPath + "/Log.txt";
+        if(!File.Exists(path)){
+            File.WriteAllText(path, "DATA\n\nCue Condition, Direct Message\n0/0\nAction Condition, Direct Message\n0/0\nCue Condition, Report\n0/0\nAction Condition, Report\n0/0\n");
+        }
+        
+        StreamReader streamReader = new StreamReader(path);
+        bool end = false;
+        while(!end){
+            var line = streamReader.ReadLine();
+            if(line.Equals("Cue Condition, Direct Message")){
+                String[] vals = streamReader.ReadLine().Split('/');
+                CDActual = vals[0].ParseLargeInteger();
+                CDTotal = vals[1].ParseLargeInteger();
+            }
+            if(line.Equals("Action Condition, Direct Message")){
+                String[] vals = streamReader.ReadLine().Split("/");
+                CDActual = vals[0].ParseLargeInteger();
+                CDTotal = vals[1].ParseLargeInteger();
+            }
+            if(line.Equals("Cue Condition, Report")){
+                String[] vals = streamReader.ReadLine().Split("/");
+                CDActual = vals[0].ParseLargeInteger();
+                CDTotal = vals[1].ParseLargeInteger();
+            }
+            if(line.Equals("Action Condition, Report")){
+                String[] vals = streamReader.ReadLine().Split("/");
+                CDActual = vals[0].ParseLargeInteger();
+                CDTotal = vals[1].ParseLargeInteger();
+                 end = true;
+            }
+        }
+    }
+
+    public void saveFile(){
+        String path = Application.dataPath + "/Log.txt";
+        StreamWriter streamWriter = new StreamWriter(path);
+        streamWriter.Write("DATA\n\nCue Condition, Direct Message\n"+CDActual+"/"+CDTotal+"\nAction Condition, Direct Message\n"+ADActual+"/"+ADTotal+"\nCue Condition, Report\n"+CRActual+"/"+CRTotal+"\nAction Condition, Report\n"+ARActual+"/"+ARTotal+"\n");
+    }   
+    
+}
--- Assets/scripts/Report register.cs
+++ Assets/scripts/Report register.cs
@@ -7,17 +7,45 @@ using UnityEngine.UI;
 public class Reportregister : MonoBehaviour
 {
     [SerializeField] TMP_Dropdown incident, offender, victim;
-    [SerializeField] Button  incidentSelect, offenderSelect, report;
 
+    private int incidentSelect, offenderSelect, victimSelect;
     public void onIncidentSelect(){
-
+        if(incident.itemText.Equals("Sexual Harrasment")){
+            incidentSelect = (int)inc.SexualHarrasment;
+        }else{
+            incidentSelect = (int)inc.other;
+        }
     }
 
     public void onOffenderSelect(){
-
+        if(incident.itemText.Equals("Sexual Harrasment")){
+            offenderSelect = (int)of.offender;
+        }else{
+            offenderSelect = (int)of.other;
+        }
     }
 
     public void OnReport(){
-        
+        if(incident.itemText.Equals("Sexual Harrasment")){
+            victimSelect = (int)vic.victim;
+        }else{
+            victimSelect = (int)vic.other;
+        }
+        if(incidentSelect+offenderSelect+victimSelect == 0){
+            FileGenerator.ARActual += 1;            
+        }
+        FileGenerator.ARTotal += 1;
     }
+}
+public enum inc{
+    SexualHarrasment = 0,
+    other = 1
+}
+public enum of{
+    offender = 0,
+    other = 1
+}
+public enum vic{
+    victim = 0,
+    other = 1
 }
