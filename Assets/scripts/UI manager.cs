fa6849cc9348e85efa758ee4225eab3d16b5c80b
Alvaro-esteche <aesteche06@gmail.com>
2025-04-13 15:28:29 -0400

    ui implemented bugs worked out

--- Assets/scripts/UI manager.cs
+++ Assets/scripts/UI manager.cs
@@ -6,16 +6,20 @@ using UnityEngine.InputSystem;
 public class UImanager : MonoBehaviour
 {
     public static UImanager instance;
-    public GameObject tutorialRep, reportHud, messageHud;
+    public GameObject tutorialRep, tutorialRecord, reportHud, messageHud;
     public InputActionReference report, message;
     void Awake()
     {
         instance = this;
-        tutorialRep.SetActive(false);
+        // tutorialRep.SetActive(false);
+        // tutorialRecord.SetActive(false);
         reportHud.SetActive(false);
         messageHud.SetActive(false);
         report.action.Enable();
         report.action.performed += ToggleReportMenu;
+        InputSystem.onDeviceChange += onDeviceChange;
+        message.action.Enable();
+        message.action.performed += ToggleRecordMenu;
         InputSystem.onDeviceChange += onDeviceChange;
     }
 
@@ -24,10 +28,16 @@ public class UImanager : MonoBehaviour
         report.action.Disable();
         report.action.performed -= ToggleReportMenu;
         InputSystem.onDeviceChange -= onDeviceChange;
+        message.action.Disable();
+        message.action.performed -= ToggleRecordMenu;
+        InputSystem.onDeviceChange -= onDeviceChange;
     }
 
     private void ToggleReportMenu(InputAction.CallbackContext context){
         reportHud.SetActive(true);
+    }
+    private void ToggleRecordMenu(InputAction.CallbackContext context){
+        messageHud.SetActive(true);
     }
 
     private void onDeviceChange(InputDevice device, InputDeviceChange change){
@@ -35,10 +45,14 @@ public class UImanager : MonoBehaviour
             case InputDeviceChange.Disconnected:
                 report.action.Disable();
                 report.action.performed -= ToggleReportMenu;
+                message.action.Disable();
+                message.action.performed -= ToggleRecordMenu;
                 break;
             case InputDeviceChange.Reconnected:
                 report.action.Enable();
                 report.action.performed += ToggleReportMenu;
+                message.action.Enable();
+                message.action.performed += ToggleRecordMenu;
                 break;
         }
     }
