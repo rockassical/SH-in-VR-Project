89c1e8655b01e7baff0105fdf8e26fefda6abc90
Alvaro-esteche <aesteche06@gmail.com>
2025-06-27 15:37:39 -0400

    changed event trigger script no more  errors

--- Assets/scripts/EventTrigger.cs
+++ Assets/scripts/EventTrigger.cs
@@ -2,43 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition.Attributes;
+using UnityEngine.UIElements;
 
 public class EventTrigger : MonoBehaviour
 {
-    public GameObject locomotion;
+    public GameObject locomotion, player, npc, rotation;
     public MeshRenderer rend;
+    public Animator playerAnimator, npcAnimator;
+    public AudioSource cutsceneAudio;
+    public Transform playerTargetPosition, npcTargetPosition;
     private bool fade;
     void OnTriggerEnter(Collider collider)
     {
-        //start cutscene here and begin animations within cinemachine?
-        //fade to black sequence so the rest of the objects and camera can move wo a new scene load
-        fade = true;
-        float transparency;
-        List<Material> list = new List<Material>();
-        while (fade)
-        {
-            rend.GetMaterials(list);
-            transparency = 0 + Time.deltaTime;
-            if (transparency > 1)
-                transparency = 1;
-            list[0].color = new Color(0, 0, 0, transparency);
-            if (transparency == 1)
-                fade = false;
-        }
-        //move objects to their places such as player and npcs
-
-
-        //undo fade to black and proceed with cinemachine after
-        fade = true;
-        {
-            rend.GetMaterials(list);
-            transparency = -Time.deltaTime;
-            if (transparency < 0)
-                transparency = 0;
-            list[0].color = new Color(0, 0, 0, transparency);
-            if (transparency == 0)
-                fade = false;
-        }
+        locomotion.SetActive(false);
+        rotation.SetActive(true);
+        StartCoroutine(StartCutscene());
     }
     //cutscene starts
     //new changes in progress to test 06/26/25
@@ -76,7 +54,7 @@ public class EventTrigger : MonoBehaviour
     IEnumerator FadeScreen(float from, float to, float duration)
     {
         float elapsed = 0f;
-        Material fadeMat = fadeRenderer.material;
+        Material fadeMat = rend.material;
         Color color = fadeMat.color;
 
         while (elapsed < duration)
