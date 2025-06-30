20a5e62d1cd1bf6601a61e56680b7eb19053886e
lismar17 <lisbeth.mtzj@gmail.com>
2025-06-30 00:26:59 -0400

    Jane Event System

--- Assets/scripts/BeginJane.cs
+++ Assets/scripts/BeginJane.cs
@@ -1,32 +1,54 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections;
 public class BeginJane : MonoBehaviour
 {
     [Header("References")]
     public Animator animator;
-    public string animationTrigger = "Click"; 
     public AudioSource audioSource;
-    public Button triggerButton;
-
+    public Transform targetPoint;
+    public float moveSpeed;
+    
     void Start()
     {
-        if (triggerButton != null)
-        {
-            triggerButton.onClick.AddListener(PlayAnimationAndAudio);
-        }
+        moveSpeed = 5;
+      
     }
-
     void PlayAnimationAndAudio()
     {
-        if (animator != null)
-        {
-            animator.SetTrigger(animationTrigger);
-        }
-
         if (audioSource != null && !audioSource.isPlaying)
         {
             audioSource.Play();
         }
+    }
+    public void ButtonActive()
+    {
+        gameObject.SetActive(true);
+        PlayAnimationAndAudio();
+        StartCoroutine(toWalk());
+        MoveToTarget();
+    }
+    void MoveToTarget()
+    {
+        if (targetPoint == null) return;
+        animator.SetTrigger("Walk");
+
+        // Move toward the target
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            targetPoint.position,
+            moveSpeed * Time.deltaTime
+        );
+
+        // Stop moving if close enough
+        if (Vector3.Distance(transform.position, targetPoint.position) <= 0.1)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+     IEnumerator toWalk() 
+    {
+        yield return new WaitForSeconds(10f);
+ 
     }
 }
